40ef083073ac9996d003db20ea728949ddda1aba
e.sadeghi <ehsun.zz@gmail.com>
2018-11-08 13:24:59 +0330

    finalize dal class

--- src/bayesian_network/bayesian_network_dal.py
+++ src/bayesian_network/bayesian_network_dal.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy
 from config.runtime_config import RuntimeConfig
 
@@ -7,16 +9,34 @@ class BayesianNetworkDal:
         self.split_char = split_char
 
     def read_data(self) -> numpy.ndarray:
-        data_path = RuntimeConfig.BASE_PROJECT_DIR + 'static/data/breast_cancer.csv'
+        """Implement based on breastCancer unformatted data set:
+        https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/unformatted-data
+        https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names
+        """
+        data_path = RuntimeConfig.BASE_PROJECT_DIR + 'static/data/binary_breast_cancer.csv'
         data_list = list()
         with open(data_path, 'r') as data_file:
             for line in data_file:
                 record = line.strip().split(self.split_char)
+                record = [int(x) for x in record]
                 data_list.append(numpy.array(record))
         data_array = numpy.array(data_list)
         return data_array
 
+    def get_processed_data(self) -> Tuple[numpy.ndarray, numpy.ndarray]:
+        """Implement based on breastCancer unformatted data set:
+        https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/unformatted-data
+        https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names
+        """
+        data_matrix = self.read_data()
+        data_matrix = data_matrix.transpose()
+        label_array = data_matrix[1]
+        label_array.astype(int)
+        feature_matrix = data_matrix[2:]
+        feature_matrix = feature_matrix.transpose()
+        return label_array, feature_matrix
+
 
 if __name__ == '__main__':
     manager = BayesianNetworkDal()
-    data = manager.read_data()
+    data = manager.get_processed_data()
